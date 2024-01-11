69ab364907f7ba9b961b2152b5ae168a3b20dbc8
Simone Carriero <simonecarriero95@hotmail.com>
2024-01-11 19:52:36 +0100

    Add Clerk settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import { ClerkProvider } from '@clerk/nextjs';
 
 export const metadata: Metadata = {
   title: 'Jobify Dev',
@@ -15,10 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={`${inter.className}  text-white bg-blue-950`}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className='text-white bg-blue-950'>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
