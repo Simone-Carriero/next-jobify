d299c3dd3e293ba120964ed9cc57392f366de6a9
Simone Carriero <simonecarriero95@hotmail.com>
2024-01-18 00:52:20 +0100

    Add theme toggle functionality to navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import Providers from './providers';
 
 export const metadata: Metadata = {
   title: 'Jobify Dev',
@@ -14,8 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang='en'>
-        <body className='text-white bg-blue-950'>{children}</body>
+      <html
+        lang='en'
+        suppressHydrationWarning>
+        <body className='text-white bg-blue-950'>
+          <Providers>{children}</Providers>
+        </body>
       </html>
     </ClerkProvider>
   );
