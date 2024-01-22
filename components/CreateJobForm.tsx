dc35db6d1f4f43d4dafffd54ed36a99cc1ac2c73
Simone Carriero <simonecarriero95@hotmail.com>
2024-01-22 20:49:11 +0100

    Update CreateJobForm component
    Create FormComponent that includes custom input and select components

--- components/CreateJobForm.tsx
+++ components/CreateJobForm.tsx
@@ -2,7 +2,7 @@
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import * as z from 'zod';
+
 import {
   dehydrate,
   HydrationBoundary,
@@ -10,47 +10,32 @@ import {
 } from '@tanstack/react-query';
 
 import { Button } from '@/components/ui/button';
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
+import { Form } from '@/components/ui/form';
 
-import { Input } from '@/components/ui/input';
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.',
-  }),
-  email: z
-    .string({
-      required_error: 'Please select an email to display.',
-    })
-    .email(),
-});
+import {
+  createAndEditJobSchema,
+  CreateAndEditJobType,
+  JobMode,
+  JobStatus,
+} from '@/utils/types';
+import { CustomFormField } from './FormComponents';
+import CustomFormSelect from './FormComponents';
 
 const CreateJobForm = () => {
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<CreateAndEditJobType>({
+    resolver: zodResolver(createAndEditJobSchema),
     defaultValues: {
-      username: '',
+      position: '',
+      company: '',
+      location: '',
+      status: JobStatus.Pending,
+      mode: JobMode.FullTime,
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: CreateAndEditJobType) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
@@ -63,107 +48,31 @@ const CreateJobForm = () => {
         className='bg-muted p-8 rounded'>
         <h2 className='capitalize text-4xl font-semibold mb-6'>Add job</h2>
         <div className='grid items-start gap-4 md:grid-cols-2 lg:grid-cols-3'>
-          <FormField
+          <CustomFormField
+            name='position'
             control={form.control}
-            name='username'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder='shadcn'
-                    {...field}
-                  />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
           />
-          <FormField
+          <CustomFormField
+            name='company'
             control={form.control}
-            name='username'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder='shadcn'
-                    {...field}
-                  />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
           />
-          <FormField
+          <CustomFormField
+            name='location'
             control={form.control}
-            name='username'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder='shadcn'
-                    {...field}
-                  />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
           />
-          <FormField
+          <CustomFormSelect
+            name='jobStatus'
+            labelText='job status'
             control={form.control}
-            name='email'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}>
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder='Select a verified email to display' />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectItem value='m@example.com'>m@example.com</SelectItem>
-                    <SelectItem value='m@google.com'>m@google.com</SelectItem>
-                    <SelectItem value='m@support.com'>m@support.com</SelectItem>
-                  </SelectContent>
-                </Select>
-
-                <FormMessage />
-              </FormItem>
-            )}
+            items={Object.values(JobStatus)}
           />
-          <FormField
+          <CustomFormSelect
+            name='jobMode'
+            labelText='job mode'
             control={form.control}
-            name='email'
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}>
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder='Select a verified email to display' />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectItem value='m@example.com'>m@example.com</SelectItem>
-                    <SelectItem value='m@google.com'>m@google.com</SelectItem>
-                    <SelectItem value='m@support.com'>m@support.com</SelectItem>
-                  </SelectContent>
-                </Select>
-
-                <FormMessage />
-              </FormItem>
-            )}
+            items={Object.values(JobMode)}
           />
+
           <Button
             className='self-end capitalize'
             type='submit'>
---
+++ components/FormComponents.tsx
@@ -0,0 +1,90 @@
+import { Input } from '@/components/ui/input';
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from '@/components/ui/form';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Control } from 'react-hook-form';
+
+type CustomFormFieldProps = {
+  name: string;
+  control: Control<any>;
+};
+
+export const CustomFormField = ({ name, control }: CustomFormFieldProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className='capitalize'>{name}</FormLabel>
+          <FormControl>
+            <Input {...field} />
+          </FormControl>
+
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+type CustomFormSelectProps = {
+  name: string;
+  control: Control<any>;
+  items: string[];
+  labelText?: string;
+};
+
+const CustomFormSelect = ({
+  name,
+  control,
+  items,
+  labelText,
+}: CustomFormSelectProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className='capitalize'>{labelText || name}</FormLabel>
+          <Select
+            onValueChange={field.onChange}
+            defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {items.map((item) => {
+                return (
+                  <SelectItem
+                    key={item}
+                    value={item}>
+                    {item}
+                  </SelectItem>
+                );
+              })}
+            </SelectContent>
+          </Select>
+
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomFormSelect;
