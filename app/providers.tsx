1a1120b11193fe8d18a0ca64266fd1eab4bd06ef
Simone Carriero <simonecarriero95@hotmail.com>
2024-01-22 19:52:22 +0100

    Update providers by adding react-query

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,31 @@
+'use client';
+
 import { ThemeProvider } from '@/components/theme-provider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
   return (
     <ThemeProvider
       attribute='class'
       defaultTheme='system'
       enableSystem
       disableTransitionOnChange>
-      {children}
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
