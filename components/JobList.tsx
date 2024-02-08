bb528e9233216a15193ac032a1171e7c65c7981f
Simone Carriero <simonecarriero95@hotmail.com>
2024-02-08 19:43:44 +0100

    Add basic pages and buttons logic

--- components/JobList.tsx
+++ components/JobList.tsx
@@ -4,6 +4,7 @@ import { getAllJobsAction } from '@/utils/actions';
 import { useQuery } from '@tanstack/react-query';
 import { useSearchParams } from 'next/navigation';
 import JobCard from './JobCard';
+import ButtonContainer from './ButtonContainer';
 
 const JobList = () => {
   const params = useSearchParams();
@@ -17,6 +18,9 @@ const JobList = () => {
   });
 
   const jobs = data?.jobs || [];
+  const count = data?.count || 0;
+  const page = data?.page || 0;
+  const totalPages = data?.totalPages || 0;
 
   if (isPending) {
     return <h2 className='text-xl'>Please Wait...</h2>;
@@ -26,6 +30,17 @@ const JobList = () => {
   }
   return (
     <>
+      <div className='flex items-center justify-between mb-8'>
+        <h2 className='text-xl font-semibold capitalize '>
+          {count} jobs found
+        </h2>
+        {totalPages < 2 ? null : (
+          <ButtonContainer
+            currentPage={page}
+            totalPages={totalPages}
+          />
+        )}
+      </div>
       <div className='grid gap-8 md:grid-cols-2'>
         {jobs.map((job) => {
           return (
